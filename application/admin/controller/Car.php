5870ab0ed1f6ad741aacdea43a466bceb7fe6a37
土豆 <potato@qq.com>
2019-04-19 16:17:30 +0800

    加入了查询数据的功能

--- application/admin/controller/Car.php
+++ application/admin/controller/Car.php
@@ -5,3 +5,18 @@
  * Date: 2019/4/19
  * Time: 15:10
  */
+namespace app\admin\controller;
+use think\Controller;
+use think\Db;
+
+session_start();
+class Car extends Controller{
+    public function Car(){
+        /**该函数用来显示所有的购物车数据库*/
+        $list=Db::table("car")->paginate(3);
+        $this->assign('list',$list);
+
+        return view();
+    }
+
+}
